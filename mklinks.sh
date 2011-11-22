cbcb6914c6f055f413e00f5af5b8340bad6f253a
Paul Cowan <paul@custardsource.com>
2011-11-22 22:19:49 +1100

    Add script to automatically populate links

---
+++ mklinks.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HOMEDIR=$HOME/
+RELPATH=${BASEPATH##$HOMEDIR}/
+
+echo "Checking symlinks in $HOME relative to $RELPATH..."
+
+cd $BASEPATH
+files=.*
+
+for file in $files; do
+  case $file in
+  . | .. )
+    ;;
+  README | mklinks.sh | .git )
+    ;;
+  * )
+    cd $HOMEDIR
+    target=$RELPATH$file
+    echo -n " Checking $file for link to $target... "
+    if [ -h $file ]; then
+      existing=$(readlink $file)
+      if [ "$existing" == "$target" ]; then
+        echo "OK (already existing)"
+      else
+        echo "ERROR: already links to $existing"
+      fi 
+    elif [ -e $file ]; then
+      echo "ERROR: already exists" 
+    else
+      echo "CREATING"
+      ln -s $target $file
+    fi
+  esac
+  
+done
